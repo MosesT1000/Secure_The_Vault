8647ce661ef4fa0e81acd1fbcff0a9e4ef084950
Moses Taplin <mosestaplin@gmail.com>
2024-11-14 12:05:04 -0500

    Initial Commit

---
+++ Index.js
@@ -0,0 +1,36 @@
+// Psudocode: Create a dialog box that says he
+
+typeof 10;
+/* Grab a toothbrush with your hand.
+Turn on the water.
+Rinse the brush part of the toothbrush with water.
+Turn off the water.
+Open the tube of toothpaste.
+Squeeze toothpaste on the brush part of the toothbrush.
+Start with the front teeth.
+Move in circular motions with the brush.
+
+console.log ("hello world")
+let numCows = 50;
+const greeting = "Hello";
+const x = "Disney World";
+console.log("Hconst numUmbrellas = 10 * 3;
+const num = 5 + 1;
+console.log(num)
+const L = "Hello";
+const K = " ";
+const M = "World";
+const str = 'Hello' + ' ' + 'World';
+let str1 = "Hello,";
+let str2 = "I am";
+let name = "Josh";ello " +  x  + "!")
+*/
+// Step 1:  Statement using let variable for alert box statement
+let str1 =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// Step 2: Calculate vault codes using different operators
+const L = 5 + 5;
+const K = 10 * 4;
+const M = 40 - 1;
+// Step 3: Put together const(s) for pop up alert box
+alert(str1 + L + -K + -M);
